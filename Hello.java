8e618fcef4ca3a9d985306e69de2585580f2defb
thewalkingdrew <69979324+thewalkingdrew@users.noreply.github.com>
2020-08-20 11:45:42 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*************************
+* Hello Java 
+*************************
+* Andrew Krakie
+* 8/20/2020
+* CMSC 255 Section 2
+**************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Andrew Krakie");
+ 		System.out.println("Hello Java");
+ 		System.out.println("Making sure that Java can be coded in and run on your computer");
+ 		System.out.println("V 8.20.2020");
+ 		System.out.println("CMSC 255 Section 2");	
+ 		System.out.println();
+ 	}
+ }
+ 	
